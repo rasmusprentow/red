3cb3a1ec3e1144eb06491be9e417278782f32a56
rprentow@gmail.com <rpentow@gmail.com>
2014-03-20 12:08:38 +0100

    Possible now to create masters and slaves services, which are externals.

--- config.py
+++ config.py
@@ -21,3 +21,13 @@ logger.info('Reading config; ' + conffile)
 ### Load config
 config = configparser.ConfigParser()
 config.read(conffile)
+
+
+def get_config(config, section, option, ctype=str, default=None): 
+    """ Auxilliary method"""
+    if default is None: 
+        ret = config.get(section, option) 
+    else: 
+        confdict = config.__dict__.get('_sections') 
+        ret = confdict.get(section).get(option, default) 
+    return ctype(ret) 

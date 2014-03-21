5db8ef1cd37586c9c8bc0c9e0ed3be5b05dab692
rprentow@gmail.com <rpentow@gmail.com>
2014-03-21 15:56:40 +0100

    Outcommented getopt in config.py

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ global config
 
 conffile='config/init.conf'
  
+ 
 # Read command line args
-myopts, args = getopt.getopt(sys.argv[1:],"c:")
+#myopts, args = getopt.getopt(sys.argv[1:],"c:")
  
 
-for o, a in myopts:
-    if o == '-c':
-        conffile=a
-
+#for o, a in myopts:
+#    if o == '-c':
+#        conffile=a
 logger = logging.getLogger('kernel')
 logger.info('Reading config; ' + conffile)
 ### Load config

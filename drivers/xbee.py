45e80e49801dceabaf6d47b6d84a798e2f181a22
Rasmus Steiniche <rasmus@steiniche.net>
2014-04-09 15:56:35 +0200

    Outcommented xbee.py

--- drivers/xbee.py
+++ drivers/xbee.py
@@ -1,23 +1,23 @@
 
 
 
-import serial, time
+#import serial, time
 
-import logging, threading
-from red.config import config, get_config
-logger = logging.getLogger(__file__)
+#import logging, threading
+#from red.config import config, get_config
+#logger = logging.getLogger(__file__)
   
 
-class XBee (object):
+#class XBee (object):
 
-    def send(class):
-        pass
+ #   def send(class):
+  #      pass
 
-    def registerCallback(callback):
-        """ Callback must take one param."""
-        pass
+   # def registerCallback(callback):
+    #    """ Callback must take one param."""
+     #   pass
 
-    def processMessage(msg):
-        """ return message of dict type. Assuming that msg is a string """
-        pass
+    #def processMessage(msg):
+     #   """ return message of dict type. Assuming that msg is a string """
+      #  pass
 
